b1fb0647d9c0bc974d1df286b192a22be6c43e21
shariful islam <sharifulb07@gmail.com>
2022-10-25 22:03:59 +0600

    c sharp variable

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// c sharp variable
+
+using System;
+
+namespace variable{
+    class Process{
+
+        // int type
+    //    public static int numberOne=-1;
+        // private static int numberTwo=2;
+        // float type
+        // public static float numberThree=20.556787648444f;
+        // // double type
+        // public static double numberFour=4.89767882343444443327f;
+        // char type
+        // public static char startLetter='A';
+        // string type
+        public static string mySpeech="I am shariful Islam speaking here.";
+        // bool type
+   
+
+      static void Main(string[]args){
+           
+            
+
+        }
+    }
+}
